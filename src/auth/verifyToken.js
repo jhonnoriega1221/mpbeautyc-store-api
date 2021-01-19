dea63f6a39685e4ff8a7c1e19aba4c04fb155ec7
jhonnoriega1221 <jhonlex17@gmail.com>
2021-01-18 20:31:54 -0500

    Se cambió el nombre de la cabecera del token

--- src/auth/verifyToken.js
+++ src/auth/verifyToken.js
@@ -15,7 +15,7 @@ function verifyUserToken(req, res, next){
 }
 
 function verifyAdminToken(req, res, next){
-    var token = req.headers['x-admin-access-token'];
+    var token = req.headers['x-access-token'];
     if(!token)
         return res.status(403).send({auth: false, message: 'No hay token.'});
 
